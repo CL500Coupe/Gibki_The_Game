8407268f0b8a7b89f4b5e2cb3dfeb8d10ddb9e54
Maciej Gronczynski <63499090+CL500Coupe@users.noreply.github.com>
2020-04-11 16:14:21 +0100

    Added loop to button 'about'

--- scratch.py
+++ scratch.py
@@ -15,6 +15,7 @@ image_width3 = 361
 image_width4 = 187
 image_width5 = 47
 image_width6 = 615
+image_width7 = 184
 
 background = pygame.image.load("background.png")
 background = pygame.transform.scale(background, (1000,800))
@@ -27,22 +28,24 @@ button_options = pygame.image.load("Sterowanie.png")
 button_options2 = pygame.image.load("Sterowanie_jasne.png")
 button_quit = pygame.image.load("Wyjd.png")
 button_quit2 = pygame.image.load("Wyjdz_jasne.png")
-
 button_about = pygame.image.load("about.png")
 button_about2 = pygame.image.load("about_jasne.png")
+button_powrot = pygame.image.load("powrot.png")
+button_powrot2 = pygame.image.load("powrot_jasne.png")
 
 x_center1 = display_width / 2 - image_width1 / 2
 x_center2 = display_width / 2 - image_width2 / 2
 x_center3 = display_width / 2 - image_width3 / 2
 x_center4 = display_width / 2 - image_width4 / 2
 x_center5 = display_width / 2 - image_width6 / 2
+x_center7 = display_width / 2 - image_width7 / 2
 
 y1= 50
 y2 = 200
 y3 = 350
 y4 = 500
 y5 = 300
-
+y7 = 650
 pygame.init()
 pygame.display.set_icon(game_icon)
 display_surface = pygame.display.set_mode((display_width,display_height)) #width-height
@@ -55,6 +58,7 @@ pygame.mixer.music.play(-1)
 pygame.mixer.music.set_volume(0.2)
 
 def about_screen():
+    #petla dzieki ktorej wyswietla sie screen about
     about = True
 
     while about:
@@ -63,12 +67,32 @@ def about_screen():
                 pygame.quit()
                 quit()
 
+
+
         display_surface.blit(background, [0, 0])
         display_surface.blit(gibki_the_game, (x_center1, y1))
         display_surface.blit(info,(x_center5,y5))
+        back_button()
+
+
+
+
+        #update screena zawsze na koncu
         pygame.display.update()
         clock.tick(15)
 
+
+def back_button(): #button POWROT
+    click = pygame.mouse.get_pressed()
+    mouse = pygame.mouse.get_pos()
+
+    if x_center7 + image_width4 > mouse[0] > x_center7 and y7 + 50 > mouse[1] > y7:
+
+        display_surface.blit(button_powrot2, (x_center7, y7))
+        if click[0] == 1:
+            menu( )
+    else:
+        display_surface.blit(button_powrot, (x_center7, y7))
 
 
 def button():
@@ -111,9 +135,22 @@ def button():
 
 
 def menu():  #main menu
-    display_surface.blit(background,[0,0])
-    display_surface.blit(gibki_the_game,(x_center1,y1))
-    button()
+    #petla dzieki ktorej dziala menu i cale to gowno sie trzyma
+    about = True
+
+
+    while about:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+
+        display_surface.blit(background,[0,0])
+        display_surface.blit(gibki_the_game,(x_center1,y1))
+        button()
+        pygame.display.update()
+        clock.tick(15)
 
 
 
